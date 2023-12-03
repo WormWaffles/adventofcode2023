777cd4243c8c672a0486fdbbeaf9c591058aac25
cbrow353 <cbrow353@uncc.edu>
2023-12-03 15:36:06 -0500

    holy moly, it works

--- Day_3/app.py
+++ Day_3/app.py
@@ -1,121 +1,37 @@
-scheme = open('Day_3/scheme.txt', 'r').read().split('\n')
+#! /usr/bin/env python3
 
-grid = []*len(scheme)
-line_len = None
-grid_width = 0
+import re
 
-sum = 0
+lines = list(open('Day_3/scheme.txt', 'r'))
+p1_total = 0
+symbols = {} 
+gears = {}
 
-nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-nono_square = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']
+for row in range(len(lines[0]) - 1):
+    for col in range(len(lines)):
+        if lines[row][col] not in '0123456789.':
+            symbols[(row, col)] = lines[row][col]
+            if lines[row][col] == '*':
+                gears[(row,col)] = []
 
-for line in scheme:
-    line = list(line)
-    line_len = len(line)
-    grid.append(line)
-    grid_width += 1
-
-idx = 0
-idy = 0
-for idx in range(line_len):
-    for idy in range(grid_width):
-        # if char is not a num or .
-        if grid[idx][idy] not in nono_square:
-            # check each position around the char
-            if grid[idx-1][idy-1] in nums:
-                current_num = grid[idx-1][idy-1]
-                if grid[idx-1][idy-2] in nums:
-                    current_num = grid[idx-1][idy-2] + current_num
-                if grid[idx-1][idy-3] in nums:
-                    current_num = grid[idx-1][idy-3] + current_num
-                if grid[idx-1][idy] in nums:
-                    current_num = current_num + grid[idx-1][idy]
-                if grid[idx-1][idy+1] in nums:
-                    current_num = current_num + grid[idx-1][idy+1]
-                print(current_num)
-                sum += int(current_num)
-            # if grid[idx][idy-1] in nums:
-            #     current_num = grid[idx][idy-1]
-            #     if grid[idx][idy-2] in nums:
-            #         current_num = grid[idx][idy-2] + current_num
-            #     if grid[idx][idy-3] in nums:
-            #         current_num = grid[idx][idy-3] + current_num
-            #     if grid[idx][idy] in nums:
-            #         current_num = current_num + grid[idx][idy]
-            #     if grid[idx][idy+1] in nums:
-            #         current_num = current_num + grid[idx][idy+1]
-            #     print(current_num)
-            #     sum += int(current_num)
-            if grid[idx+1][idy-1] in nums:
-                current_num = grid[idx+1][idy-1]
-                if grid[idx+1][idy-2] in nums:
-                    current_num = grid[idx+1][idy-2] + current_num
-                if grid[idx+1][idy-3] in nums:
-                    current_num = grid[idx+1][idy-3] + current_num
-                if grid[idx+1][idy] in nums:
-                    current_num = current_num + grid[idx+1][idy]
-                if grid[idx+1][idy+1] in nums:
-                    current_num = current_num + grid[idx+1][idy+1]
-                print(current_num)
-                sum += int(current_num)
-            if grid[idx-1][idy] in nums:
-                current_num = grid[idx-1][idy]
-                if grid[idx-1][idy-1] in nums:
-                    current_num = grid[idx-1][idy-1] + current_num
-                if grid[idx-1][idy-2] in nums:
-                    current_num = grid[idx-1][idy-2] + current_num
-                if grid[idx-1][idy+1] in nums:
-                    current_num = current_num + grid[idx-1][idy+1]
-                if grid[idx-1][idy+2] in nums:
-                    current_num = current_num + grid[idx-1][idy+2]
-                print(current_num)
-                sum += int(current_num)
-            if grid[idx+1][idy] in nums:
-                current_num = grid[idx+1][idy]
-                if grid[idx+1][idy-1] in nums:
-                    current_num = grid[idx+1][idy-1] + current_num
-                if grid[idx+1][idy-2] in nums:
-                    current_num = grid[idx+1][idy-2] + current_num
-                if grid[idx+1][idy+1] in nums:
-                    current_num = current_num + grid[idx+1][idy+1]
-                if grid[idx+1][idy+2] in nums:
-                    current_num = current_num + grid[idx+1][idy+2]
-                print(current_num)
-                sum += int(current_num)
-            if grid[idx-1][idy+1] in nums:
-                current_num = grid[idx-1][idy+1]
-                if grid[idx-1][idy] in nums:
-                    current_num = grid[idx-1][idy] + current_num
-                if grid[idx-1][idy-1] in nums:
-                    current_num = grid[idx-1][idy-1] + current_num
-                if grid[idx-1][idy+2] in nums:
-                    current_num = current_num + grid[idx-1][idy+2]
-                if grid[idx-1][idy+3] in nums:
-                    current_num = current_num + grid[idx-1][idy+3]
-                sum += int(current_num)
-                print(current_num)
-            if grid[idx][idy+1] in nums:
-                current_num = grid[idx][idy+1]
-                if grid[idx][idy] in nums:
-                    current_num = grid[idx][idy] + current_num
-                if grid[idx][idy-1] in nums:
-                    current_num = grid[idx][idy-1] + current_num
-                if grid[idx][idy+2] in nums:
-                    current_num = current_num + grid[idx][idy+2]
-                if grid[idx][idy+3] in nums:
-                    current_num = current_num + grid[idx][idy+3]
-                print(current_num)
-                sum += int(current_num)
-            if grid[idx+1][idy+1] in nums:
-                current_num = grid[idx+1][idy+1]
-                if grid[idx+1][idy] in nums:
-                    current_num = grid[idx+1][idy] + current_num
-                if grid[idx+1][idy-1] in nums:
-                    current_num = grid[idx+1][idy-1] + current_num
-                if grid[idx+1][idy+2] in nums:
-                    current_num = current_num + grid[idx+1][idy+2]
-                if grid[idx+1][idy+3] in nums:
-                    current_num = current_num + grid[idx+1][idy+3]
-                print(current_num)
-                sum += int(current_num)
-print(sum)
+for row_num, row in enumerate(lines):
+    for c in re.finditer(r'\d+', row):
+        possibilities = []
+        for i in range(c.start() - 1, c.end() + 1):
+            possibilities.append((row_num - 1, i))
+            possibilities.append((row_num, i))
+            possibilities.append((row_num + 1, i))
+        valid = False
+        for p in possibilities:
+            if p in symbols:
+                valid = True
+                if p in gears:
+                    gears[p].append(int(c.group()))
+        if valid:
+            p1_total += int(c.group())
+print('Part One -', p1_total)
+p2_total = 0
+for g in gears:
+    if len(gears[g]) == 2:
+        p2_total += (gears[g][0] * gears[g][1])
+print('Part Two -', p2_total)
