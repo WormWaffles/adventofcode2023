f4e090651682a3b814c95011b6bde27c684697e6
WormWaffles <colin8297@gmail.com>
2023-12-05 22:13:09 -0500

    finished and fast

--- Day_5/app.py
+++ Day_5/app.py
@@ -1,47 +1,22 @@
-import sys
+from functools import reduce
 
-almanac = open('Day_5/seeds.txt', 'r').read().split('\n\n')
-seeds = almanac[0].split(' ')[1:]
+seeds, *mappings = open('Day_5/seeds.txt').read().split('\n\n')
+seeds = list(map(int, seeds.split()[1:]))
 
-
-# progress bar
-print("Calculating, this is gonna be a while...\n")
-def progress(count, total, status=''):
-    bar_len = 60
-    filled_len = int(round(bar_len * count / float(total)))
-
-    percents = round(100.0 * count / float(total), 1)
-    bar = '=' * filled_len + '-' * (bar_len - filled_len)
-
-    sys.stdout.write('[{0}] {1}%  {2}\r'.format(bar, percents, status))
-    sys.stdout.flush()
-
-
-# PART 2 ---------------
-maps = []
-for i in range(7):
-    maps.append(almanac[i+1].split('\n')[1:])
-
-current_seeds = []
-lowest = 9223372036854775807
-i = 0
-while i < len(seeds):
-    for idx in range(int(seeds[i+1])):
-        current_seeds.append(int(seeds[i]) + idx)
-    # calc here
-    for i in range(len(maps)):
-        progress(0, 100, status='')
-        for j in range(len(current_seeds)):
-            progress(j, len(current_seeds), status='')
-            # calculate each map here
-            for map in maps[i]:
-                if int(current_seeds[j]) >= int(map.split(' ')[1]) and int(current_seeds[j]) <= int(map.split(' ')[1]) + int(map.split(' ')[2]) - 1:
-                    current_seeds[j] = str(int(map.split(' ')[0]) + int(current_seeds[j]) - int(map.split(' ')[1]))
+def lookup(inputs, mapping):
+    for start, length in inputs:
+        while length > 0:
+            for m in mapping.split('\n')[1:]:
+                dst, src, len = map(int, m.split())
+                delta = start - src
+                if delta in range(len):
+                    len = min(len - delta, length)
+                    yield (dst + delta, len)
+                    start += len
+                    length -= len
                     break
-    for pos in current_seeds:
-        if int(pos) < lowest:
-            lowest = int(pos)
-    current_seeds = []
-    i += 2
-progress(100, 100, status='')
-print("\n\nLowest location is: " + str(lowest))
+            else: yield (start, length); break
+
+print(*[min(reduce(lookup, mappings, s))[0] for s in [
+    zip(seeds, [1] * len(seeds)),
+    zip(seeds[0::2], seeds[1::2])]])
