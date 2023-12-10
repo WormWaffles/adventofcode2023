24b86ef3734b6839b0927d2e7bb7521cdc6883fd
cbrow353 <cbrow353@uncc.edu>
2023-12-10 12:39:44 -0500

    finished part 2

--- Day_9/app.py
+++ Day_9/app.py
@@ -8,8 +8,19 @@ def find_next(arr):
     arr2 = [int(arr[i+1]) - int(arr[i]) for i in range(len(arr)-1)]
     return int(arr[len(arr)-1]) + find_next(arr2)
 
-sum = 0
-for code in codes:
-    sum += find_next(code)
+def find_prev(arr):
+    # if all arr is 0
+    if arr.count(0) == len(arr):
+        return 0
+    arr2 = [int(arr[i+1]) - int(arr[i]) for i in range(len(arr)-1)]
+    return int(arr[0]) - find_prev(arr2)
 
-print(sum)
+sum_next = 0
+sum_prev = 0
+for code in codes:
+    sum_next += find_next(code)
+    sum_prev += find_prev(code)
+
+
+print(sum_next)
+print(sum_prev)
