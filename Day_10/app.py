1d2c3ed223adde9da6346e6b5f46c6f4c854aa50
cbrow353 <cbrow353@uncc.edu>
2023-12-10 12:52:10 -0500

    setup2

--- Day_10/app.py
+++ Day_10/app.py
@@ -1,4 +1,11 @@
 loop = open('Day_10/test_loop.txt').read().split('\n')
-loop = [int(i) for i in loop]
+
+# find 'S' in loop (loop is a list of strings)
+y = 0
+for i in range(len(loop)):
+    x = loop[i].find('S')
+    if x != -1:
+        y = i
+        break
 
 print(loop)
